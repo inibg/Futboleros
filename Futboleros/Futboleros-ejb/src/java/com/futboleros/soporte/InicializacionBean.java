3ed7118785689dee01e27e1ac948174f3a61fb02
Ignacio <inibg2@gmail.com>
2016-05-07 11:42:31 -0300

    Inicializa datos por defecto

---
+++ Futboleros/Futboleros-ejb/src/java/com/futboleros/soporte/InicializacionBean.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.futboleros.soporte;
+
+import com.futboleros.club.ClubBean;
+import com.futboleros.dto.ClubDto;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author inibg
+ */
+@Singleton
+@LocalBean
+@Startup
+public class InicializacionBean {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    @EJB
+    ClubBean clubBean;
+    
+    @PostConstruct
+    public void inicializarDb(){
+       System.out.println("EjecutaInicializar");
+       cargarClubes();
+        
+    }
+    
+    private void cargarClubes(){
+        ClubDto nuevo = new ClubDto(0L, "Danubio");
+        clubBean.agregarClub(nuevo);
+        nuevo = new ClubDto(0L, "Defensor");
+        clubBean.agregarClub(nuevo);
+    }
+    
+    public InicializacionBean(){
+        System.out.println("Constructor");
+    }
+}
