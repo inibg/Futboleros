09511742ff95c9e0ec0457b5f95fa10292b52ed6
Ignacio <inibg2@gmail.com>
2016-06-11 17:42:51 -0300

    Se agrega busqueda de partidos por id

--- Futboleros/Futboleros-ejb/src/java/com/futboleros/partido/PartidoBean.java
+++ Futboleros/Futboleros-ejb/src/java/com/futboleros/partido/PartidoBean.java
@@ -6,14 +6,12 @@
 package com.futboleros.partido;
 
 import com.futboleros.club.ClubBean;
-import com.futboleros.dto.ClubDto;
 import com.futboleros.dto.PartidoDto;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -33,52 +31,32 @@ public class PartidoBean {
     private ClubBean clubBean;
     
     protected Partido toEntity(PartidoDto dto){
-        
-         /*Club local = clubBean.toEntity(dto.getClubLocal());
-        Club visitante = clubBean.toEntity(dto.getClubVisitante());*/
-      /*  Partido ent = new Partido(dto.getId(), local, visitante, /*dto.getFechaPartido(),*/
-          //  dto.getGolesLocal(), dto.getGolesVisitante());
-        //return ent;*/
-        
-        
-     //   Club local = clubBean.toEntity(dto.getClubLocal());
-        //Club visitante = clubBean.toEntity(dto.getClubVisitante());
-        
-        Partido ent = new Partido (dto.getId(), dto.getClubLocal(), dto.getClubVisitante(),dto.getFechaPartido(), dto.getGolesLocal(), dto.getGolesVisitante());
-       
+        Partido ent = new Partido (dto.getId(), dto.getClubLocal(), dto.getClubVisitante(),
+                dto.getFechaPartido(), dto.getGolesLocal(), dto.getGolesVisitante());
         return ent; 
-                
-                
-                
     }
     
     protected PartidoDto toDto(Partido ent){
-      /*  ClubDto local = clubBean.toDto(ent.getClubLocal());
-        ClubDto visitante = clubBean.toDto(ent.getClubVisitante());
-        PartidoDto dto = new PartidoDto(ent.getId(), local, visitante, /*ent.getFechaPartido(),*/
-          /*  ent.getGolesLocal(), ent.getGolesVisitante());*/
-       /* return dto;*/
-        
-       // ClubDto local = clubBean.toDto(ent.getClubLocal());
-         
-        PartidoDto dto = new PartidoDto(ent.getId(), ent.getClubLocal(), ent.getClubVisitante(), ent.getFechaPartido(),
-            ent.getGolesLocal(), ent.getGolesVisitante());
-        if (dto!=null){
-            return dto;
-        }else{
-            return null;
-        }
-        
+        PartidoDto dto = new PartidoDto(ent.getId(), ent.getClubLocal(), ent.getClubVisitante(),
+                ent.getFechaPartido(),
+        ent.getGolesLocal(), ent.getGolesVisitante());
+        return dto;
     }
     
     public Long agregarPartido(PartidoDto dto){
         Partido nuevo = toEntity(dto);
-        
-            em.persist(nuevo);
-          return nuevo.getId();
-      
+        em.persist(nuevo);
+        return nuevo.getId();
     }
     
+    public PartidoDto obtenerPartidoPorId(Long id){
+        Partido buscado = em.find(Partido.class, id);
+        if (buscado==null){
+            return null;
+        }else{
+            return toDto(buscado);
+        }
+    }
     
 
 }
