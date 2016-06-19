41ea88a263340a30d71c866602a7289c2de5b86b
Ignacio Berretta <inibg2@gmail.com>
2016-06-19 12:04:34 -0300

    Se acomoda el codigo de acuerdo al checkstyle de google

--- Futboleros/Futboleros-ejb/src/java/com/futboleros/configuracion/InicializacionBean.java
+++ Futboleros/Futboleros-ejb/src/java/com/futboleros/configuracion/InicializacionBean.java
@@ -1,79 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.futboleros.configuracion;
 
 import com.futboleros.club.ClubBean;
 import com.futboleros.club.ClubDto;
-import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
-import javax.ejb.Singleton;
-import javax.ejb.LocalBean;
-import javax.ejb.Startup;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- *
- * @author inibg
- */
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+
 @Singleton
 @LocalBean
 @Startup
 public class InicializacionBean {
 
-    @PersistenceContext
-    private EntityManager em;
-    
-    @EJB
-    ClubBean clubBean;
-        
-    private static final Logger logger = LogManager.getLogger(InicializacionBean.class);
-    
-    @PostConstruct
-    public void inicializarDb(){
-        logger.info("EjecutaInicializar");
-        cargarClubes();
-    }
-    
-    
-    private void cargarClubes(){
+  @PersistenceContext
+  private EntityManager em;
    
-        ClubDto nuevo = new ClubDto(0L, "Peñarol");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Nacional");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Danubio");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Defensor");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Wanderers");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Cerro");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Plaza Colonia");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Rentistas");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Fenix");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Liverpool");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Tanque Sisley");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Juventud de Las Piedras");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "River Plate");
-        clubBean.agregarClub(nuevo);
-        nuevo = new ClubDto(0L, "Villa Teresa");
-        clubBean.agregarClub(nuevo);
-    }
+  @EJB
+  ClubBean clubBean;
+        
+  private static final Logger logger = LogManager.getLogger(InicializacionBean.class);
     
-    public InicializacionBean(){
-    }
+  @PostConstruct
+  public void inicializarDb() {
+    logger.info("EjecutaInicializar");
+    cargarClubes();
+  }
+    
+    
+  private void cargarClubes() {
+    ClubDto nuevo = new ClubDto(0L, "Peñarol");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Nacional");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Danubio");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Defensor");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Wanderers");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Cerro");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Plaza Colonia");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Rentistas");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Fenix");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Liverpool");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Tanque Sisley");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Juventud de Las Piedras");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "River Plate");
+    clubBean.agregarClub(nuevo);
+    nuevo = new ClubDto(0L, "Villa Teresa");
+    clubBean.agregarClub(nuevo);
+  }
+    
+  public InicializacionBean() {}
     
 }
